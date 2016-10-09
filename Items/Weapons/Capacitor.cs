8046d42445c7d7ca8dea9aad255153169ff3cda6
Flash Kirby <flashkirby@gmail.com>
2016-10-09 19:00:51 +0100

    Port Capacitor to new system

--- Items/Weapons/Capacitor.cs
+++ Items/Weapons/Capacitor.cs
@@ -7,27 +7,7 @@ namespace WeaponOut.Items.Weapons
 {
     public class Capacitor : ModItem
     {
-        //minimum transform stats, usually the melee one
-        const int damage = 17;
-        const int damageM = 30;
-        const float knockBack = 4f;
-        const float knockBackM = 0f;
-        const int sound = 1;
-        const int soundM = 0;
-        const int useAnimation = 21;
-        const int useAnimationM = 15;
-        const int useTime = 21;
-        const int useTimeM = 15;
-        const int mana = 16;
-        const int shoot = ProjectileID.FrostBoltStaff;
-        const float shootSpeed = 14f;
-
-        public int damageMod;
-        public float knockBackMod;
-        public int useAnimationMod;
-        public int manaMod;
-        public float shootSpeedMod;
-
+        HelperDual dual;
         public override void SetDefaults()
         {
             item.name = "Capacitor";
@@ -37,13 +17,37 @@ namespace WeaponOut.Items.Weapons
             item.scale = 1.15f;
 
             item.autoReuse = true;
-            //generate a default style, where melee stats take precedent
-            magicDefaults();
+
+            item.useSound = 1;
+            item.useStyle = 1; //swing
+            item.useAnimation = 16;
+            item.useTime = 15;
+
+            item.melee = true; //melee damage
+            item.damage = 17;
+            item.knockBack = 4f;
+
             Item.staff[item.type] = true; //rotate weapon, as it is a staff
-            meleeDefaults(true);
 
             item.rare = 2;
             item.value = 20000;
+
+            dual = new HelperDual(item, true); //prioritise magic defaults
+            dual.UseSound = 0;
+            dual.UseStyle = 5;
+            dual.UseAnimation = 21;
+            dual.UseTime = 21;
+
+            dual.Magic = true;
+            dual.NoMelee = true;
+            dual.Damage = 30;
+            dual.KnockBack = 0f;
+
+            dual.Mana = 16;
+            dual.Shoot = ProjectileID.FrostBoltStaff; //staff one is magic, sword one is melee
+            dual.ShootSpeed = 14f;
+
+            dual.setValues(false, true);
         }
         public override void AddRecipes()
         {
@@ -53,6 +57,23 @@ namespace WeaponOut.Items.Weapons
             recipe.AddTile(TileID.Anvils);
             recipe.SetResult(this);
             recipe.AddRecipe();
+        }
+
+        public override bool AltFunctionUse(Player player) { return true; }
+        public override void UseStyle(Player player)
+        {
+            dual.UseStyleMultiplayer(player);
+            if (player.altFunctionUse > 0) PlayerFX.modifyPlayerItemLocation(player, -6, 0);
+        }
+        public override bool CanUseItem(Player player)
+        {
+            dual.CanUseItem(player);
+            return base.CanUseItem(player);
+        }
+        public override void HoldStyle(Player player)
+        {
+            dual.HoldStyle(player);
+            base.HoldStyle(player);
         }
 
         public override void OnHitNPC(Player player, NPC target, int damage, float knockBack, bool crit)
@@ -73,99 +94,6 @@ namespace WeaponOut.Items.Weapons
                 Main.dust[d].noGravity = true;
             }
         }
-        
-        public override void UseStyle(Player player)
-        {
-            if(player.altFunctionUse == 2) PlayerFX.modifyPlayerItemLocation(player, -6, 0);
-        }
-
-
-
-
-
-
-
-        #region (type 1) Crazy Value Swapping Transform Stuff (Should probably make this its own class at some point)
-
-        /// <summary>
-        /// Here are some standard settings for switching between melee and magic
-        /// </summary>
-        /// <param name="keepMagicValues"></param>
-        private void meleeDefaults(bool keepMagicValues = false)
-        {
-            item.useStyle = 1; //swing
-            item.noMelee = false;
-
-            item.melee = true; //melee damage
-            item.magic = false;
-            item.damage = damage + damageMod;
-            item.knockBack = knockBack + knockBackMod;
-
-            item.useSound = sound;
-            item.useAnimation = useAnimation + useAnimationMod;
-            item.useTime = useTime + useAnimationMod;
-
-            if (!keepMagicValues)
-            {
-                item.mana = 0;
-                item.shoot = 0;
-                item.shootSpeed = 0;
-            }
-        }
-        /// <summary>
-        /// The general design has the magic aspect being more powerful but slower/less frequent
-        /// </summary>
-        private void magicDefaults()
-        {
-            item.useStyle = 5; //aim
-            item.noMelee = true;
-
-            item.magic = true; //magic damage
-            item.melee = false;
-            item.damage = damageM + damageMod;
-            item.knockBack = knockBackM + knockBackMod;
-
-            item.useSound = soundM;
-            item.useAnimation = useAnimationM + useAnimationMod;
-            item.useTime = useTimeM + useAnimationMod;
-
-            item.mana = mana + manaMod;
-            item.shoot = shoot;
-            item.shootSpeed = shootSpeed + shootSpeedMod;
-        }
-        public override void PostReforge()
-        {
-            damageMod = item.damage - damage;
-            knockBackMod = item.knockBack - knockBack;
-            useAnimationMod = item.useAnimation - useAnimation;
-            manaMod = item.mana - mana;
-            shootSpeedMod = item.shootSpeed - shootSpeed;
-        }
-        public override bool AltFunctionUse(Player player)
-        {
-            return true;
-        }
-        public override bool CanUseItem(Player player)
-        {
-            if (player.altFunctionUse > 0)
-            {
-                magicDefaults();
-            }
-            else
-            {
-                meleeDefaults();
-            }
-            return base.CanUseItem(player);
-        }
-        public override void HoldItem(Player player)
-        {
-            if (player.itemAnimation == 0)
-            {
-                magicDefaults();
-                meleeDefaults(true);
-            }
-        }
-        #endregion
 
     }
 }
