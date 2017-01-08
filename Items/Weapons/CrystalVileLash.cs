e776618666a7803e90d0e53c835db396ae76740a
Flash Kirby <flashkirby@gmail.com>
2017-01-08 22:26:17 +0000

    Add crystal vilelash code

---
+++ Items/Weapons/CrystalVileLash.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace WeaponOut.Items.Weapons
+{
+    /// <summary>
+    /// Not the garian sword :P
+    /// </summary>
+    public class CrystalVileLash : ModItem
+    {
+        public override bool Autoload(ref string name, ref string texture, IList<EquipType> equips)
+        {
+            return ModConf.enableWhips;
+        }
+        
+        public override void SetDefaults()
+        {
+            item.name = "Crystal Vilelash";
+			item.width = 34;
+			item.height = 34;
+
+            item.useStyle = 5;
+            item.useAnimation = 28;
+            item.useTime = 28;
+            item.UseSound = SoundID.Item19;
+            item.noUseGraphic = true;
+            item.noMelee = true;
+            item.autoReuse = true;
+            
+            item.melee = true;
+            item.channel = true;
+            item.damage = 35;
+            item.crit = 36; //crit chance on whips increase crit damage instead
+            item.knockBack = 1.5f;
+            item.shoot = mod.ProjectileType(this.GetType().Name);
+            item.shootSpeed = 1f; //projectile length
+
+            item.rare = 5;
+            item.value = Item.sellPrice(0, 7, 0, 0);
+        }
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.CrystalVileShard, 1);
+            recipe.AddIngredient(ItemID.SoulofLight, 5);
+            recipe.AddTile(TileID.MythrilAnvil);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            Projectile.NewProjectile(position.X, position.Y, speedX, speedY, type, damage, knockBack, player.whoAmI, 0, 
+                Main.rand.Next(-100, 100) * 0.001f * player.gravDir); //whip swinging
+            return false;
+        }
+    }
+}
---
+++ Projectiles/CrystalVileLash.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.DataStructures;
+using System;
+
+namespace WeaponOut.Projectiles
+{
+    public class CrystalVileLash : ModProjectile
+    {
+        public const float whipLength = 32f;
+        public const bool whipSoftSound = false;
+        public const int handleHeight = 24;
+        public const int chainHeight = 14;
+        public const int partHeight = 14;
+        public const int tipHeight = 16;
+        public override void SetDefaults()
+        {
+            projectile.name = "Crystal Vilelash";
+            projectile.width = 18;
+            projectile.height = 18;
+            projectile.scale = 1.2f;
+            projectile.alpha = 255;
+            projectile.aiStyle = 75;
+            projectile.penetrate = -1;
+
+            projectile.alpha = 0;
+            projectile.hide = true;
+            projectile.extraUpdates = 3;
+            projectile.light = 0.3f;
+
+            projectile.friendly = true;
+            projectile.melee = true;
+            projectile.tileCollide = false;
+            projectile.ignoreWater = true;
+            projectile.usesLocalNPCImmunity = true;
+        }
+
+        public override void AI()
+        {
+            Vector2 endPos = BaseWhip.WhipAI(projectile, whipLength);
+            //Dust along projectile
+            Dust dust2 = Main.dust[Dust.NewDust(endPos, projectile.width, projectile.height, Main.rand.Next(68, 71), 0f, 0f, 200, default(Color), 1.3f)];
+            dust2.noGravity = true;
+            dust2.velocity += projectile.localAI[0].ToRotationVector2();
+
+            Lighting.AddLight(endPos, projectile.light, projectile.light * 0.3f, projectile.light);
+        }
+
+        #region BaseWhip Stuff
+        
+        public override void ModifyHitNPC(NPC target, ref int damage, ref float knockback, ref bool crit, ref int hitDirection)
+        {
+            BaseWhip.ModifyHitAny(projectile, ref damage, ref knockback, ref crit);
+        }
+        public override void ModifyHitPlayer(Player target, ref int damage, ref bool crit)
+        {
+            BaseWhip.ModifyHitAny(projectile, ref damage, ref crit);
+        }
+        public override void ModifyHitPvp(Player target, ref int damage, ref bool crit)
+        {
+            BaseWhip.ModifyHitAny(projectile, ref damage, ref crit);
+        }
+
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            BaseWhip.OnHitAny(projectile, target, crit, whipSoftSound);
+        }
+        public override void OnHitPlayer(Player target, int damage, bool crit)
+        {
+            BaseWhip.OnHitAny(projectile, crit, whipSoftSound);
+        }
+        public override void OnHitPvp(Player target, int damage, bool crit)
+        {
+            BaseWhip.OnHitAny(projectile, crit, whipSoftSound);
+        }
+
+        public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
+        {
+            return BaseWhip.Colliding(projectile, targetHitbox);
+        }
+
+        public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
+        {
+            return BaseWhip.PreDraw(projectile, handleHeight, chainHeight, partHeight, tipHeight, 12, true);
+        }
+        
+        #endregion
+    
+    }
+}
