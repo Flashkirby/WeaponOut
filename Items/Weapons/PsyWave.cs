4f7aa7de64673d5cbfdcfdf91e84a94528227dd7
Flash Kirby <flashkirby@gmail.com>
2017-01-09 02:59:05 +0000

    Add psywave

---
+++ Items/Weapons/PsyWave.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace WeaponOut.Items.Weapons
+{
+    public class PsyWave : ModItem
+    {
+        public override bool Autoload(ref string name, ref string texture, IList<EquipType> equips)
+        {
+            return ModConf.enableBasicContent;
+        }
+
+        public override void SetDefaults()
+        {
+            item.CloneDefaults(ItemID.WaterBolt);
+            item.name = "Psy Wave";
+            item.toolTip = "Cast a psionic orb";
+            
+            item.useAnimation = 15;
+            item.useTime = 15;
+            item.UseSound = SoundID.Item24;
+            
+            item.damage = 40;
+            item.knockBack = 0f;
+            item.mana = 2;
+            item.shoot = mod.ProjectileType(this.GetType().Name);
+            item.shootSpeed = 1.5f;
+
+
+            item.rare = 8;
+            item.value = Item.sellPrice(0, 6, 0, 0);
+        }
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.SpellTome, 1);
+            recipe.AddIngredient(ItemID.Ectoplasm, 8);
+            recipe.AddTile(TileID.Bookcases);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
---
+++ Projectiles/PsyWave.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using System;
+
+namespace WeaponOut.Projectiles
+{
+    public class PsyWave : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            projectile.name = "Psychic Wave";
+            projectile.width = 32;
+            projectile.height = 32;
+            Main.projFrames[projectile.type] = 4;
+
+            projectile.penetrate = -1;
+            projectile.alpha = 50;
+
+            projectile.friendly = true;
+            projectile.magic = true;
+            projectile.tileCollide = true;
+            projectile.ignoreWater = true;
+        }
+
+        public override void AI()
+        {
+            Player player = Main.player[projectile.owner];
+            if((player.itemAnimation == 0 && projectile.ai[1] == 0f)
+                || projectile.ai[1] == 1f
+                || projectile.ai[0] > 480)
+            {
+                // Break effect
+                projectile.ai[1] = 1f;
+                projectile.netUpdate = true;
+            }
+
+            if (projectile.ai[1] > 0f)
+            { 
+                //disappearing
+                projectile.friendly = false;
+                projectile.tileCollide = false;
+                projectile.alpha += 3;
+                projectile.ai[1]++;
+                if (projectile.ai[1] > 66)
+                {
+                    projectile.timeLeft = 0;
+                }
+            }else
+            {
+                projectile.position += player.velocity;
+            }
+
+            projectile.direction = 1;
+            if (projectile.velocity.X < 0) projectile.direction = -1;
+            projectile.ai[0]++;
+
+            // Set frame and direction
+            projectile.spriteDirection = projectile.direction;
+            projectile.frameCounter++;
+            if(projectile.frameCounter > 4)
+            {
+                projectile.frame++;
+                if(projectile.frame >= Main.projFrames[projectile.type])
+                {
+                    projectile.frame = 0;
+                }
+            }
+        }
+
+        public override void TileCollideStyle(ref int width, ref int height, ref bool fallThrough)
+        {
+            width = projectile.width / 2;
+            height = projectile.height / 2;
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            if (projectile.ai[1] < 1f) projectile.ai[1] = 1;
+            projectile.velocity = projectile.oldVelocity;
+            projectile.tileCollide = false;
+            return false;
+        }
+
+        public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
+        {
+
+            Texture2D texture = Main.projectileTexture[projectile.type];
+            int frameCount = Main.projFrames[projectile.type];
+
+            int frameHeight = texture.Height / frameCount;
+
+            // Flip Horziontally
+            SpriteEffects spriteEffect = SpriteEffects.None;
+            spriteEffect = SpriteEffects.None;
+            if (projectile.spriteDirection < 0)
+            {
+                spriteEffect = SpriteEffects.FlipHorizontally;
+            }
+
+            
+
+            spriteBatch.Draw(
+                texture,
+                projectile.Center - Main.screenPosition,
+                new Rectangle(0, projectile.frame * frameHeight, texture.Width, frameHeight),
+                new Color(projectile.Opacity, projectile.Opacity, projectile.Opacity, projectile.Opacity),
+                0f,
+                new Vector2(texture.Width / 2, frameHeight / 2),
+                projectile.scale,
+                spriteEffect,
+                0f);
+            return false;
+        }
+    }
+}
