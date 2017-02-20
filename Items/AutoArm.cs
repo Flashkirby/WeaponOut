20dffee9ec1c37d0a408f9e9f052149abeae5050
Flash Kirby <flashkirby@gmail.com>
2017-02-20 20:25:22 +0000

    Add auto arm

---
+++ Items/AutoArm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+
+using Microsoft.Xna.Framework;
+
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace WeaponOut.Items
+{
+    public class AutoArm : ModItem
+    {
+        public override bool Autoload(ref string name, ref string texture, IList<EquipType> equips)
+        {
+            return ModConf.enableBasicContent;
+        }
+
+        public override void SetDefaults()
+        {
+            item.name = "Voodoo Arm";
+            item.toolTip = "Causes weapons to autoswing whilst in inventory";
+            item.toolTip2 = "'It shudders constantly'";
+            item.width = 28;
+            item.height = 28;
+            item.rare = 1;
+            item.value = Item.sellPrice(0, 0, 5, 0);
+        }
+        public override void AddRecipes()
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                ModRecipe recipe = new ModRecipe(mod);
+                recipe.AddIngredient(ItemID.ZombieArm, 1);
+                if (i == 0)
+                { recipe.AddIngredient(ItemID.CopperWatch, 1); }
+                else
+                { recipe.AddIngredient(ItemID.TinWatch, 1); }
+                recipe.AddTile(TileID.WorkBenches);
+                recipe.SetResult(this, 1);
+                recipe.AddRecipe();
+            }
+        }
+        public override void UpdateInventory(Player player)
+        {
+            if (!player.HeldItem.autoReuse && !player.HeldItem.channel)
+            {
+                if (player.itemAnimation == 0)
+                {
+                    player.releaseUseItem = true;
+                }
+            }
+        }
+    }
+}
