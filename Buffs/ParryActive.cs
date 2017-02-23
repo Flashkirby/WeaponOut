695bd717e2d993f5b3ae9a1f2dcf8061132546fc
Flash Kirby <flashkirby@gmail.com>
2017-02-23 11:36:36 +0000

    Hide parry bonus in tip when not equipped

--- Buffs/ParryActive.cs
+++ Buffs/ParryActive.cs
@@ -13,7 +13,12 @@ namespace WeaponOut.Buffs
         public override void SetDefaults()
         {
             Main.buffName[Type] = "Parrying Moment";
-            Main.buffTip[Type] = "Ready for parry bonus";
+            Main.buffTip[Type] = "Provides a bonus effect to fist weapons with parry";
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            Main.buffTip[Type] = "Provides a bonus effect to fist weapons with parry";
         }
     }
 }
