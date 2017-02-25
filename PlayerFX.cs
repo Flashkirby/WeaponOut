01623f49ced160491a36d2df71e4f7d94ddaf368
Flash Kirby <flashkirby@gmail.com>
2017-02-25 13:53:44 +0000

    Fix hand show issue with fists

--- PlayerFX.cs
+++ PlayerFX.cs
@@ -1016,8 +1016,8 @@ namespace WeaponOut
             {
                 if (player.HeldItem.useStyle == Items.Weapons.UseStyles.FistStyle.useStyle)
                 {
-                    if (player.handon > 0) player.handon = player.HeldItem.handOnSlot;
-                    if (player.handoff> 0) player.handoff = player.HeldItem.handOffSlot;
+                    if (player.HeldItem.handOnSlot > 0) player.handon = player.HeldItem.handOnSlot;
+                    if (player.HeldItem.handOffSlot > 0) player.handoff = player.HeldItem.handOffSlot;
                 }
             }
         }
