851a62ec395a74a684480bc5ec8452acbafe3588
Flash Kirby <flashkirby@gmail.com>
2017-03-05 15:58:38 +0000

    Add scrap salvo

--- NPCs/NpcFX.cs
+++ NPCs/NpcFX.cs
@@ -24,12 +24,20 @@ namespace WeaponOut.NPCs
                     //add puzzling cutter if hardmode
                     if (Main.hardMode)
                     {
-                        try
-                        {
-                            shop.item[nextSlot].SetDefaults(mod.GetItem("PuzzlingCutter").item.type);
-                            nextSlot++;
-                        }
-                        catch (Exception e) { }
+                        shop.item[nextSlot].SetDefaults(mod.ItemType<Items.Weapons.Whips.PuzzlingCutter>());
+                        nextSlot++;
+                    }
+                }
+            }
+            if (ModConf.enableBasicContent)
+            {
+                if (type == NPCID.ArmsDealer)
+                {
+                    //add scrap salvo after mech
+                    if (Main.hardMode && NPC.downedPlantBoss)
+                    {
+                        shop.item[nextSlot].SetDefaults(mod.ItemType<Items.Weapons.ScrapSalvo>());
+                        nextSlot++;
                     }
                 }
             }
