504bcdf42d9d83795ccf2759ca08417f643883d1
Flash Kirby <flashkirby@gmail.com>
2017-08-17 23:58:49 +0100

    Add meteor combo fist

---
+++ Buffs/MeteorShield.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace WeaponOut.Buffs
+{
+    public class MeteorShield : ModBuff
+    {
+        public override bool Autoload(ref string name, ref string texture)
+        {
+            return ModConf.enableFists;
+        }
+
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("Meteor Shield");
+            Description.SetDefault("Damage taken reduced by 20%");
+            Main.buffNoTimeDisplay[Type] = true;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.endurance += 0.2f;
+
+            Vector2 position;
+            for (int i = 0; i < 3; i++)
+            {
+                // meteoric shield effect
+                float angle = Main.rand.Next((int)(-Math.PI * 10000), (int)(Math.PI * 10000)) / 10000f;
+                position = ProjFX.CalculateCircleVector(player, angle, 22f);
+                Dust d = Main.dust[Dust.NewDust(position, 0, 0, 6)];
+                d.rotation += i * d.velocity.X;
+                d.velocity = player.velocity / 2;
+                d.noGravity = true;
+                d.noLight = true;
+                d.scale *= 1.5f;
+            }
+        }
+    }
+}
---
+++ Items/Weapons/Fists/KnucklesMeteor.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace WeaponOut.Items.Weapons.Fists
+{
+    [AutoloadEquip(EquipType.HandsOn, EquipType.HandsOff)]
+    public class KnucklesMeteor : ModItem
+    {
+        public override bool Autoload(ref string name)
+        {
+            return ModConf.enableFists;
+        }
+        public static int comboEffect = 0;
+        public static int projectileID = 0;
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Comet Fu");
+            Tooltip.SetDefault(
+                "<right> consumes combo to meteor strike!\n" +
+                "Combo grants a protective barrier\n" +
+                "'Space CQC is explicitly stated to be whatever you claim it to be'"); //これは宇宙CQC! sorry MPT no kanas are supported
+            comboEffect = ModPlayerFists.RegisterComboEffectID(ComboEffects);
+            projectileID = mod.ProjectileType<Projectiles.SpiritComet>();
+        }
+        public override void SetDefaults()
+        {
+            item.useStyle = ModPlayerFists.useStyle;
+            item.autoReuse = true;
+            item.useAnimation = 19; // Combos can increase speed by 30-50% since it halves remaining attack time
+
+            item.width = 20;
+            item.height = 20;
+            item.damage = 15;
+            item.knockBack = 2.5f;
+            item.UseSound = SoundID.Item7;
+            
+            item.shootSpeed = 6f;
+
+            item.tileBoost = 5; // For fists, this is the combo power
+
+            item.value = Item.sellPrice(0, 0, 24, 0);
+            item.rare = 2;
+            item.noUseGraphic = true;
+            item.melee = true;
+        }
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.MeteoriteBar, 5);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
+        { ModPlayerFists.ModifyTooltips(tooltips, item); }
+
+        public override bool CanUseItem(Player player)
+        {
+            if (player.altFunctionUse == 0)
+            {   // Short dash brings up to max default speed.
+                player.GetModPlayer<ModPlayerFists>().
+                SetDashOnMovement(5f, 12f, 0.992f, 0.96f, true, 0);
+            }
+            return true;
+        }
+        public override bool AltFunctionUse(Player player)
+        {
+            return player.GetModPlayer<ModPlayerFists>().
+                AltFunctionCombo(player, comboEffect);
+        }
+        /// <summary> The method called during a combo. Use for ongoing dust and gore effects. </summary>
+        public static void ComboEffects(Player player, bool initial)
+        {
+            if (initial)
+            {
+                player.itemAnimation = player.itemAnimationMax + 12;
+                Main.PlaySound(SoundID.DD2_SkyDragonsFurySwing, player.position);
+            }
+            // Charging
+            Rectangle r = ModPlayerFists.UseItemGraphicbox(player, 16, 20);
+            if (player.itemAnimation > player.itemAnimationMax)
+            {
+                // Charge effect
+                Dust d = Main.dust[Dust.NewDust(r.TopLeft(), 16, 16, 6, 0, 0, 100, default(Color), 2f)];
+                d.velocity /= 2;
+                d.noGravity = true;
+            }
+            // Initial throw
+            else if (player.itemAnimation == player.itemAnimationMax)
+            {
+                // Higher pitch
+                Main.PlaySound(SoundID.Item88, (int)player.position.X, (int)player.position.Y);
+                // Spawn projectile
+                if (Main.netMode == 0 || (Main.netMode == 1 && player.whoAmI == Main.myPlayer))
+                {
+                    Vector2 velocity = WeaponOut.CalculateNormalAngle(player.Center, Main.MouseWorld);
+                    velocity *= player.HeldItem.shootSpeed;
+                    Projectile.NewProjectile(player.Center, velocity, projectileID,
+                        (int)(player.HeldItem.damage * player.meleeDamage * 5f),
+                        player.GetWeaponKnockback(player.HeldItem, player.HeldItem.knockBack),
+                        player.whoAmI);
+                }
+            }
+            else
+            {
+                // Punch effect
+                Dust d = Main.dust[Dust.NewDust(r.TopLeft(), 16, 16, 6, 0, 0, 100, default(Color), 1.3f)];
+                d.velocity = 1.4f * ModPlayerFists.GetFistVelocity(player);
+                d.noGravity = true;
+            }
+        }
+
+        public override void UseItemHitbox(Player player, ref Rectangle hitbox, ref bool noHitbox)
+        {
+            ModPlayerFists.UseItemHitbox(player, ref hitbox, 20, 10f, 3f);
+        }
+
+        //Combo
+        public override void HoldItem(Player player)
+        {
+            ModPlayerFists mpf = player.GetModPlayer<ModPlayerFists>();
+            if (mpf.IsComboActive)
+            {
+                player.AddBuff(mod.BuffType<Buffs.MeteorShield>(), 30);
+            }
+        }
+
+        // Melee Effect
+        public override void MeleeEffects(Player player, Rectangle hitbox)
+        {
+            Rectangle r = ModPlayerFists.UseItemGraphicbox(player, 4, 20);
+            Vector2 velocity = ModPlayerFists.GetFistVelocity(player);
+            Dust d = Main.dust[Dust.NewDust(r.TopLeft(), r.Width, r.Height, 174, player.velocity.X, player.velocity.Y)];
+            d.noGravity = true;
+            d.velocity += velocity * -2f;
+        }
+    }
+}
