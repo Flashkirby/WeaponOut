fa1c8d93b46b3509cac5e32235e5e90cc0ede2a9
Flash Kirby <flashkirby@gmail.com>
2017-08-25 00:59:26 +0100

    Add cerulean claws

---
+++ Buffs/Flurry.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace WeaponOut.Buffs
+{
+    public class Flurry : ModBuff
+    {
+        public override bool Autoload(ref string name, ref string texture)
+        {
+            return ModConf.enableFists;
+        }
+
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("Flurry");
+            Description.SetDefault("100% increased melee attack speed and always attacking");
+            Main.buffNoTimeDisplay[Type] = true;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.meleeSpeed += 1f;
+
+            // From lunar emblems
+            if (!player.HeldItem.autoReuse && !player.HeldItem.channel)
+            {
+                if (player.itemAnimation == 0)
+                {
+                    player.releaseUseItem = true;
+                }
+            }
+            else
+            {
+                player.controlUseItem = true;
+            }
+        }
+    }
+}
---
+++ Items/Weapons/Fists/KnucklesDungeon.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace WeaponOut.Items.Weapons.Fists
+{
+    [AutoloadEquip(EquipType.HandsOn, EquipType.HandsOff)]
+    public class KnucklesDungeon : ModItem
+    {
+        public override bool Autoload(ref string name)
+        {
+            return ModConf.enableFists;
+        }
+        public static int comboEffect = 0;
+        public static int buffID = 0;
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Cerulean Claws");
+            Tooltip.SetDefault(
+                "<right> consumes combo to unleash a flurry of strikes\n" +
+                "Combo grants 25% increased melee attack speed");
+            comboEffect = ModPlayerFists.RegisterComboEffectID(ComboEffects);
+            buffID = mod.BuffType<Buffs.Flurry>();
+        }
+        public override void SetDefaults()
+        {
+            item.melee = true;
+            item.damage = 21;
+            item.useAnimation = 16; // Combos can increase speed by 30-50% since it halves remaining attack time
+            item.knockBack = 3f;
+            item.tileBoost = 15; // For fists, we read this as the combo power
+
+            item.value = Item.sellPrice(0, 0, 15, 0);
+            item.rare = 2;
+
+            item.UseSound = SoundID.Item19;
+            item.useStyle = ModPlayerFists.useStyle;
+            item.autoReuse = true;
+            item.noUseGraphic = true;
+            item.width = 20;
+            item.height = 20;
+        }
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.GoldenKey, 1);
+            recipe.AddIngredient(ItemID.Bone, 20);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
+        { ModPlayerFists.ModifyTooltips(tooltips, item); }
+
+        public override bool CanUseItem(Player player)
+        {
+            if (player.altFunctionUse == 0)
+            {   // Short dash brings up to max default speed.
+                player.GetModPlayer<ModPlayerFists>().
+                SetDashOnMovement(5f, 12f, 0.992f, 0.96f, true, 0);
+            }
+            return true;
+        }
+        public override bool AltFunctionUse(Player player)
+        {
+            return player.GetModPlayer<ModPlayerFists>().
+                AltFunctionCombo(player, comboEffect);
+        }
+        /// <summary> The method called during a combo. Use for ongoing dust and gore effects. </summary>
+        public static void ComboEffects(Player player, bool initial)
+        {
+            if (initial)
+            {
+                player.itemAnimation = player.itemAnimationMax + 20;
+                player.velocity.X = 0;
+                player.velocity.Y = player.velocity.Y == 0f ? 0f : -5.5f;
+                Main.PlaySound(SoundID.DD2_SkyDragonsFurySwing, player.position);
+            }
+            // Charging
+            Rectangle r = ModPlayerFists.UseItemGraphicbox(player, 16, 20);
+            if (player.itemAnimation > player.itemAnimationMax)
+            {
+                // Charge effect
+                for (int i = 0; i < 3; i++)
+                {
+                    Dust d = Main.dust[Dust.NewDust(r.TopLeft(), 16, 16, 172, 0, 0, 0, default(Color), 0.7f)];
+                    d.fadeIn = 1.2f;
+                    d.position -= d.velocity * 20f;
+                    d.velocity *= 1.5f;
+                    d.noGravity = true;
+                }
+            }
+            // Initial throw
+            else if (player.itemAnimation == player.itemAnimationMax)
+            {
+                // Higher pitch
+                player.AddBuff(buffID, 60, false);
+            }
+        }
+
+        public override void UseItemHitbox(Player player, ref Rectangle hitbox, ref bool noHitbox)
+        {
+            ModPlayerFists.UseItemHitbox(player, ref hitbox, 22, 11.7f, 3f, 12f);
+        }
+
+        //Combo
+        public override void HoldItem(Player player)
+        {
+            ModPlayerFists mpf = player.GetModPlayer<ModPlayerFists>();
+            if (mpf.IsComboActive)
+            {
+                player.meleeSpeed += 0.25f;
+            }
+        }
+
+        // Melee Effect
+        public override void MeleeEffects(Player player, Rectangle hitbox)
+        {
+            Rectangle r = ModPlayerFists.UseItemGraphicbox(player, 2, 22);
+            Vector2 velocity = ModPlayerFists.GetFistVelocity(player);
+            Vector2 perpendicular = velocity.RotatedBy(Math.PI / 2);
+            Vector2 pVelo = (player.position - player.oldPosition);
+            // Claw like effect
+            for (int y = -1; y < 2; y++)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    Dust d = Main.dust[Dust.NewDust(r.TopLeft() + perpendicular * y * 5, r.Width, r.Height, 172,
+                        0, 0, 0, default(Color), 0.7f)];
+                    d.velocity /= 8;
+                    d.velocity += new Vector2(velocity.X, velocity.Y) + pVelo;
+                    d.noGravity = true;
+                }
+            }
+        }
+    }
+}
