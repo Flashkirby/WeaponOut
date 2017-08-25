af6b7bc6dba74bf3c8e94ab502b296aaeb32bc24
Flash Kirby <flashkirby@gmail.com>
2017-08-25 01:33:38 +0100

    Add demon frenzy buff

---
+++ Buffs/DemonFrenzy.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace WeaponOut.Buffs
+{
+    public class DemonFrenzy : ModBuff
+    {
+        public static readonly int POWERFACTOR = 1;
+
+        public override bool Autoload(ref string name, ref string texture)
+        {
+            return ModConf.enableFists;
+        }
+
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("Demon Frenzy");
+            Description.SetDefault("Damage dealt and taken increased by POWERFACTOR%");
+            Main.buffNoTimeDisplay[Type] = true;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            ModPlayerFists mpf = player.GetModPlayer<ModPlayerFists>();
+            float power = POWERFACTOR * 0.01f * mpf.ComboCounter;
+            player.endurance -= power;
+            player.thrownDamage += power;
+            player.meleeDamage += power;
+            player.rangedDamage += power;
+            player.magicDamage += power;
+            player.minionDamage += power;
+        }
+
+        public override void ModifyBuffTip(ref string tip, ref int rare)
+        {
+            ModPlayerFists mpf = Main.LocalPlayer.GetModPlayer<ModPlayerFists>();
+            int power = POWERFACTOR * mpf.ComboCounter;
+            tip = tip.Replace("POWERFACTOR%", power + "%");
+            if (power >= 100) rare = 4;
+            else if (power >= 50) rare = 3;
+            else if (power >= 25) rare = 2;
+            else if (power >= 10) rare = 1;
+        }
+    }
+}
