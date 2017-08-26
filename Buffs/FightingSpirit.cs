cdd5fd8100b86ce010fee3f4a8dfc8f73536143a
Flash Kirby <flashkirby@gmail.com>
2017-08-26 01:36:44 +0100

    Add fighting sash accessory

---
+++ Buffs/FightingSpirit.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace WeaponOut.Buffs
+{
+    public class FightingSpirit : ModBuff
+    {
+        public override bool Autoload(ref string name, ref string texture)
+        {
+            return ModConf.enableFists;
+        }
+
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("Puglist's Resolve");
+            Description.SetDefault("Heals LIFE% life at the end of a combo");
+        }
+
+        public int lifeStored = 0;
+        public int lastLife = 0;
+        public bool startRecording = false;
+
+        public int LifeRestorable(Player player)
+        { return Math.Min((int)(player.statLifeMax2 * 0.25f), lifeStored); }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+
+            ModPlayerFists mpf = player.GetModPlayer<ModPlayerFists>(mod);
+            if (mpf.ComboCounter > 0)
+            {
+                // Set time to be visible
+                player.buffTime[buffIndex] = mpf.comboTimerMax - mpf.comboTimer;
+
+                if (!startRecording)
+                {
+                    lastLife = player.statLife;
+                    startRecording = true;
+                }
+
+                // Lost health? Records lost amount
+                if (player.statLife < lastLife)
+                { lifeStored += lastLife - player.statLife; }
+
+                lastLife = player.statLife;
+
+                lifeStored = LifeRestorable(player);
+            }
+            else
+            {
+                if (mpf.ComboFinishedFrame > 0)
+                {
+                    if (!player.moonLeech && lifeStored > 0)
+                    {
+                        Main.PlaySound(2, -1, -1, 4, 0.3f, 0.2f); // mini heal effect
+                        player.HealEffect(lifeStored, false);
+                        player.statLife += lifeStored;
+                        player.statLife = Math.Min(player.statLife, player.statLifeMax2);
+                        NetMessage.SendData(MessageID.SpiritHeal, -1, -1, null, player.whoAmI, lifeStored);
+                    }
+                }
+
+                startRecording = false;
+                lifeStored = 0;
+            }
+        }
+
+        public override void ModifyBuffTip(ref string tip, ref int rare)
+        {
+            tip = tip.Replace("LIFE%", "" + lifeStored);
+        }
+    }
+}
---
+++ Items/Accessories/FightingSash.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace WeaponOut.Items.Accessories
+{
+    [AutoloadEquip(EquipType.Waist)]
+    public class FightingSash : ModItem
+    {
+        public override bool Autoload(ref string name) { return ModConf.enableFists; }
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Pugilist Sash"); //Ceratopsian Shield
+            Tooltip.SetDefault(
+                "Restores lost life at the end of a combo\n" +
+                "Restores up to 25% of maximum life\n" + 
+                "Grants 20 damage knockback immunity");
+        }
+        public override void SetDefaults()
+        {
+            item.width = 24;
+            item.height = 28;
+            item.rare = 1;
+            item.accessory = true;
+            item.value = Item.buyPrice(0, 1, 0, 0);
+        }
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.Silk, 2);
+            recipe.AddIngredient(ItemID.FallenStar, 5);
+            recipe.AddIngredient(ItemID.GoldCoin, 1);
+            recipe.AddTile(TileID.Loom);
+            recipe.SetResult(this, 1);
+            recipe.AddRecipe();
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            PlayerFX modPlayer = player.GetModPlayer<PlayerFX>(mod);
+            modPlayer.DamageKnockbackThreshold += 20;
+
+            if (player.whoAmI == Main.myPlayer)
+            {
+                ModPlayerFists mpf = player.GetModPlayer<ModPlayerFists>(mod);
+                if (mpf.ComboCounter > 0)
+                {
+                    player.AddBuff(mod.BuffType<Buffs.FightingSpirit>(), 2);
+                }
+            }
+        }
+    }
+}
