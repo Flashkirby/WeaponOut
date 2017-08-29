17c990b010452cb7b015b5cfeb983db8c976aef4
Flash Kirby <flashkirby@gmail.com>
2017-08-29 12:31:12 +0100

    Add boxing set

---
+++ Items/Armour/FistBoxingHelmet.cs
@@ -0,0 +1,93 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Localization;
+
+namespace WeaponOut.Items.Armour
+{
+    [AutoloadEquip(EquipType.Head)]
+    public class FistBoxingHelmet : ModItem
+    {
+        public override bool Autoload(ref string name) { return ModConf.enableFists; }
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Boxing Helmet");
+        }
+        public override void SetDefaults()
+        {
+            item.defense = 3;
+            item.value = Item.sellPrice(0, 0, 10, 0);
+
+            item.width = 18;
+            item.height = 18;
+        }
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.Silk, 10);
+            recipe.AddIngredient(ItemID.Cactus, 5);
+            recipe.AddTile(TileID.Loom);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+
+        public override void DrawHair(ref bool drawHair, ref bool drawAltHair)
+        {
+            drawHair = true;
+        }
+
+        private byte armourSet = 0;
+        public override bool IsArmorSet(Item head, Item body, Item legs)
+        {
+            armourSet = 0;
+            if (body.type == mod.ItemType<FistPowerBody>() &&
+                legs.type == mod.ItemType<FistPowerLegs>())
+            {
+                armourSet = 1;
+                return true;
+            }
+            else if (body.type == mod.ItemType<FistDefBody>() &&
+                legs.type == mod.ItemType<FistDefLegs>())
+            {
+                armourSet = 2;
+                return true;
+            }
+            else if (body.type == mod.ItemType<FistSpeedBody>() &&
+                legs.type == mod.ItemType<FistSpeedLegs>())
+            {
+                armourSet = 2;
+                return true;
+            }
+            else if (body.type == ItemID.Gi)
+            {
+                armourSet = 4;
+                return true;
+            }
+            return false;
+        }
+        
+        public override void UpdateArmorSet(Player player)
+        {
+            ModPlayerFists mpf = ModPlayerFists.Get(player);
+            switch (armourSet)
+            {
+                case 1:
+                    player.setBonus = Language.GetTextValue("ArmorSetBonus.MetalTier1");
+                    player.statDefense += 2;
+                    break;
+                case 2:
+                    player.setBonus = Language.GetTextValue("ArmorSetBonus.MetalTier2");
+                    player.statDefense += 3;
+                    break;
+                case 3:
+                    player.setBonus = Language.GetTextValue("ArmorSetBonus.Platinum");
+                    player.statDefense += 4;
+                    break;
+                case 4:
+                    player.setBonus = "Parrying with fists is easier";
+                    mpf.longParry = true;
+                    break;
+            }
+        }
+    }
+}
---
+++ Items/Armour/FistSpeedLegs.cs
@@ -0,0 +1,58 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace WeaponOut.Items.Armour
+{
+    [AutoloadEquip(EquipType.Legs)]
+    public class FistSpeedLegs : ModItem
+    {
+        public override bool Autoload(ref string name)
+        {
+            if (ModConf.enableFists)
+            {
+                if (!Main.dedServ)
+                {
+                    // Add the female leg variant
+                    mod.AddEquipTexture(null, EquipType.Legs, "FistSpeedLegs_Female", "WeaponOut/Items/Armour/FistSpeedLegs_FemaleLegs");
+                }
+                return true;
+            }
+            return false;
+        }
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Boxing Shorts");
+            Tooltip.SetDefault("5% increased movement speed\n" +
+                "Reduces combo power cost by 1");
+        }
+        public override void SetDefaults()
+        {
+            item.defense = 0;
+            item.value = Item.sellPrice(0, 0, 6, 0);
+
+            item.width = 18;
+            item.height = 18;
+        }
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.Silk, 12);
+            recipe.AddTile(TileID.Loom);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+
+        public override void SetMatch(bool male, ref int equipSlot, ref bool robes)
+        {
+            if (!male) equipSlot = mod.GetEquipSlot("FistSpeedLegs_Female", EquipType.Legs);
+        }
+
+        public override void UpdateEquip(Player player)
+        {
+            player.moveSpeed += 0.05f;
+            ModPlayerFists mpf = ModPlayerFists.Get(player);
+            mpf.comboCounterMaxBonus -= 1;
+        }
+    }
+}
