e360aa36ad80c0927444c8cb1dfff26e68477bdf
Flash Kirby <flashkirby@gmail.com>
2017-08-30 02:38:44 +0100

    Add Hardmode armour body graphics

---
+++ Items/Armour/HighDefLegs.cs
@@ -0,0 +1,59 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace WeaponOut.Items.Armour
+{
+    [AutoloadEquip(EquipType.Legs)]
+    public class HighDefLegs : ModItem
+    {
+        public override bool Autoload(ref string name)
+        {
+            if (ModConf.enableFists)
+            {
+                if (!Main.dedServ)
+                {
+                    // Add the female leg variant
+                    mod.AddEquipTexture(null, EquipType.Legs, "HighDefLegs_Female", "WeaponOut/Items/Armour/HighDefLegs_FemaleLegs");
+                }
+                return true;
+            }
+            return false;
+        }
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Shaolin Trousers");
+            Tooltip.SetDefault("5% increased melee critical strike chance\n" + 
+                "Increases length of combo by 1 second");
+        }
+        public override void SetDefaults()
+        {
+            item.defense = 0;
+            item.value = Item.sellPrice(0, 0, 6, 0);
+            item.rare = 6;
+
+            item.width = 18;
+            item.height = 18;
+        }
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.Silk, 12);
+            recipe.AddTile(TileID.Loom);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+
+        public override void SetMatch(bool male, ref int equipSlot, ref bool robes)
+        {
+            if (!male) equipSlot = mod.GetEquipSlot("HighDefLegs_Female", EquipType.Legs);
+        }
+
+        public override void UpdateEquip(Player player)
+        {
+            player.meleeCrit += 5;
+            ModPlayerFists mpf = ModPlayerFists.Get(player);
+            mpf.comboResetTimeBonus += 60;
+        }
+    }
+}
---
+++ Items/Armour/HighPowerLegs.cs
@@ -0,0 +1,43 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace WeaponOut.Items.Armour
+{
+    [AutoloadEquip(EquipType.Legs)]
+    public class HighPowerLegs : ModItem
+    {
+        public override bool Autoload(ref string name) { return ModConf.enableFists; }
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Sahyun Trousers");
+            Tooltip.SetDefault("5% increased melee damage\n" + 
+                "100% increased divekick damage and knockback");
+        }
+        public override void SetDefaults()
+        {
+            item.defense = 0;
+            item.value = Item.sellPrice(0, 0, 6, 0);
+            item.rare = 6;
+
+            item.width = 18;
+            item.height = 18;
+        }
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.Silk, 12);
+            recipe.AddTile(TileID.Loom);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+
+        public override void UpdateEquip(Player player)
+        {
+            player.meleeDamage += 0.05f;
+            ModPlayerFists mpf = ModPlayerFists.Get(player);
+            mpf.divekickDamage += 1f;
+            mpf.divekickKnockback += 1f;
+        }
+    }
+}
