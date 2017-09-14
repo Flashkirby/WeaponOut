a25e288d2aebf5761852fc15996c2b4015bd7a00
Flash Kirby <flashkirby@gmail.com>
2017-09-14 22:51:25 +0100

    Add crystal gauntlet

---
+++ Items/Weapons/Fists/GlovesCrystal.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Graphics.Shaders;
+
+namespace WeaponOut.Items.Weapons.Fists
+{
+    [AutoloadEquip(EquipType.HandsOn, EquipType.HandsOff)]
+    public class GlovesCrystal : ModItem
+    {
+        public override bool Autoload(ref string name) { return ModConf.enableFists; }
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Crystal Gauntlet");
+            Tooltip.SetDefault(
+                "<right> to parry incoming damage\n" +
+                "Counterstrike unleashes crystal beams that build combo\n" +
+                "Combo causes damage to refract towards nearby enemies");
+        }
+        public override void SetDefaults()
+        {
+            item.melee = true;
+            item.damage = 71;
+            item.useAnimation = 26; // 30%-50% reduction
+            item.knockBack = 4f;
+            item.tileBoost = 18; // Combo Power
+
+            item.value = Item.sellPrice(0, 1, 50, 0);
+            item.rare = 4;
+
+            item.UseSound = SoundID.Item18;
+            item.useStyle = ModPlayerFists.useStyle;
+            item.autoReuse = true;
+            item.noUseGraphic = true;
+            item.width = 20;
+            item.height = 20;
+        }
+        const int fistHitboxSize = 28;
+        const float fistDashSpeed = 8f;
+        const float fistDashThresh = 12f;
+        const float fistJumpVelo = 13.5f; // http://rextester.com/OIY60171
+        public bool AltStats(Player p) { return p.GetModPlayer<ModPlayerFists>().parryBuff; }
+        const int altHitboxSize = 32;
+        const float altDashSpeed = 14f;
+        const float altDashThresh = 12f;
+        const float altJumpVelo = 17.6f;
+        const int parryActive = 25;
+        const int parryCooldown = 15;
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.UnicornHorn, 2);
+            recipe.AddIngredient(ItemID.CrystalShard, 30);
+            recipe.AddTile(TileID.MythrilAnvil);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+
+        // Parry & Combo
+        public override void OnHitNPC(Player player, NPC target, int damage, float knockBack, bool crit)
+        {
+            ModPlayerFists mpf = player.GetModPlayer<ModPlayerFists>();
+            if (AltStats(player))
+            {
+                if (mpf.GetParryBuff() >= 0) mpf.ClearParryBuff();
+
+                foreach (NPC npc in Main.npc)
+                {
+                    if (!npc.active || npc.friendly || npc.whoAmI == target.whoAmI) continue;
+                    if (npc.Distance(player.Center) < 256)
+                    {
+                        if(player.CanHit(npc))
+                        {
+                            Projectile.NewProjectile(npc.Center, new Vector2(-player.direction),
+                                mod.ProjectileType<Projectiles.SpiritBeam>(), (int)(damage * 0.33f), 0f, player.whoAmI);
+                        }
+                    }
+                }
+            }
+            if (mpf.IsComboActiveItemOnHit)
+            {
+                float distance = 128;
+                NPC nextTarget = null;
+                foreach (NPC npc in Main.npc)
+                {
+                    if (!npc.active || npc.friendly || npc.whoAmI == target.whoAmI) continue;
+                    float ndist = npc.Distance(player.Center);
+                    if (ndist < distance)
+                    {
+                        nextTarget = npc;
+                        distance = ndist;
+                    }
+                }
+                if (nextTarget != null)
+                {
+                    Projectile.NewProjectile(nextTarget.Center, new Vector2(-player.direction),
+                        mod.ProjectileType<Projectiles.SpiritBeam>(), damage, knockBack, player.whoAmI);
+                }
+            }
+        }
+
+        #region Hardmode Parry Base
+        public override bool CanUseItem(Player player)
+        {
+            if (!AltStats(player))
+            {
+                player.GetModPlayer<ModPlayerFists>().
+                SetDashOnMovement(fistDashSpeed, fistDashThresh, 0.992f, 0.96f, true, 0);
+            }
+            else
+            {
+                player.GetModPlayer<ModPlayerFists>().
+                SetDashOnMovement(altDashSpeed, altDashThresh, 0.992f, 0.96f, true, 0);
+            }
+            return true;
+        }
+        public override bool AltFunctionUse(Player player)
+        {
+            return player.GetModPlayer<ModPlayerFists>().
+                AltFunctionParryDash(player, parryActive, parryCooldown, fistJumpVelo - fistJumpVelo / 4, fistDashSpeed / 2f, fistDashThresh / 2);
+        }
+        public override void UseItemHitbox(Player player, ref Rectangle hitbox, ref bool noHitbox)
+        {
+            if (!AltStats(player))
+            {
+                ModPlayerFists.UseItemHitbox(player, ref hitbox, fistHitboxSize, fistJumpVelo, 4f, 14f);
+            }
+            else
+            {
+                ModPlayerFists.UseItemHitbox(player, ref hitbox, altHitboxSize, altJumpVelo, 4f, 14f);
+            }
+        }
+        #endregion
+
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
+        { ModPlayerFists.ModifyTooltips(tooltips, item); }
+    }
+}
---
+++ Projectiles/SpiritBeam.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using System;
+
+namespace WeaponOut.Projectiles
+{
+    /// <summary>
+    /// Goes in either direction based on ai[1]
+    /// </summary>
+    public class SpiritBeam : ModProjectile
+    {
+        public override bool Autoload(ref string name) { return ModConf.enableFists; }
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Crystal Beam");
+        }
+        public override void SetDefaults()
+        {
+            projectile.width = 4;
+            projectile.height = 4;
+
+            projectile.friendly = true;
+            projectile.melee = true;
+            projectile.ignoreWater = true;
+            projectile.timeLeft = 2;
+        }
+
+        public override void AI()
+        {
+            Vector2 start = projectile.Center;
+            if (Main.player[projectile.owner].active) start = Main.player[projectile.owner].Center;
+            for (int i = 0; i < 30; i++)
+            {
+                Dust d = Main.dust[Dust.NewDust(new Vector2(
+                    MathHelper.Lerp(projectile.Center.X, start.X, i / 30f),
+                    MathHelper.Lerp(projectile.Center.Y, start.Y, i / 30f)
+                    ), projectile.width, projectile.height,
+                    DustID.BlueCrystalShard + Main.rand.Next(3), 0, 0, 100, default(Color), 1.2f)];
+                d.velocity *= 0.1f;
+                d.noGravity = true;
+            }
+        }
+
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            if (Main.player[projectile.owner].active)
+            {
+                Main.player[projectile.owner].GetModPlayer<ModPlayerFists>().ModifyComboCounter(1);
+            }
+        }
+
+        public override void Kill(int timeLeft)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Dust d = Main.dust[Dust.NewDust(projectile.position, projectile.width, projectile.height,
+                     DustID.BlueCrystalShard + Main.rand.Next(3), 0, 0, 100, default(Color), 1.2f)];
+                d.velocity *= 2 + (0.1f * i);
+                d.noGravity = true;
+            }
+            Main.PlaySound(2, (int)projectile.position.X, (int)projectile.position.Y, 27, 0.6f, 0.3f);
+        }
+    }
+}
