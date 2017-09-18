b8bd880236929dc16958b1f56f07b76f0906082e
Flash Kirby <flashkirby@gmail.com>
2017-09-18 23:46:22 +0100

    Fix bad checks for lag targetting

--- NPCs/NpcFX.cs
+++ NPCs/NpcFX.cs
@@ -160,7 +160,7 @@ namespace WeaponOut.NPCs
         #region The last straw against player-snapping bosses :(
         public override bool PreAI(NPC npc)
         {
-            if (npc.lifeMax < 2000 || npc.damage == 0) return true; // we don't deal in small fry and clones
+            if (npc.lifeMax < 2000 || !npc.chaseable || npc.npcSlots <= 0f) return true; // we don't deal in small fry and clones
             if (npc.boss || npc.GetBossHeadTextureIndex() >= 0)
             {
                 PlayerFX pfx;
@@ -176,7 +176,7 @@ namespace WeaponOut.NPCs
         }
         public override void PostAI(NPC npc)
         {
-            if (npc.lifeMax < 2000) return; // stuff like eater of worlds
+            if (npc.lifeMax < 2000 || !npc.chaseable || npc.npcSlots <= 0f) return;  // stuff like eater of worlds
             if (npc.boss || npc.GetBossHeadTextureIndex() >= 0)
             {
                 PlayerFX pfx;
