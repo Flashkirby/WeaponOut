ef248edddccfd82222ef74055d4c49096d22dabf
Flash Kirby <flashkirby@gmail.com>
2017-10-08 02:12:26 +0100

    Add platnera/golem accessory skeleton

---
+++ Items/Accessories/GolemHeart.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace WeaponOut.Items.Accessories
+{
+    [AutoloadEquip(EquipType.Neck)]
+    public class GolemHeart : ModItem
+    {
+        public override bool Autoload(ref string name) { return ModConf.enableFists; }
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Solar Spark");
+            Tooltip.SetDefault(
+                "Reduces combo power cost by 3\n" +
+                "Automatically consume combo power when below 25% life to heal");
+        }
+        public override void SetDefaults()
+        {
+            item.width = 22;
+            item.height = 22;
+            item.rare = 8;
+            item.accessory = true;
+            item.value = Item.sellPrice(0, 1, 0, 0);
+            item.expert = true;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            ModPlayerFists.Get(player).comboCounterMaxBonus -= 3;
+            //player.GetModPlayer<PlayerFX>().heartDropper = true;
+        }
+    }
+}
---
+++ Items/Accessories/LifeRoot.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace WeaponOut.Items.Accessories
+{
+    public class LifeRoot : ModItem
+    {
+        public override bool Autoload(ref string name) { return ModConf.enableFists; }
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Symbiotic Root");
+            Tooltip.SetDefault(
+                "Hearts drop more frequently and heal 5 more life");
+        }
+        public override void SetDefaults()
+        {
+            item.width = 22;
+            item.height = 22;
+            item.rare = 7;
+            item.accessory = true;
+            item.value = Item.sellPrice(0, 1, 0, 0);
+            item.expert = true;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.GetModPlayer<PlayerFX>().heartDropper = true;
+            //player.GetModPlayer<PlayerFX>().heartBuff = true;
+        }
+    }
+}
