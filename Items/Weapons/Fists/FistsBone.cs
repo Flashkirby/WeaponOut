5e531ae204643197cab31f0a138d4be7ae64c321
Flash Kirby <flashkirby@gmail.com>
2017-10-10 15:55:57 +0100

    Add flavour text for phalanx

--- Items/Weapons/Fists/FistsBone.cs
+++ Items/Weapons/Fists/FistsBone.cs
@@ -20,7 +20,8 @@ namespace WeaponOut.Items.Weapons.Fists
             Tooltip.SetDefault(
                 "<right> to dash through enemies\n" +
                 "Dash grants 30% increased melee damage\n" + 
-                "Combo summons a backup skeleton");
+                "Combo summons a backup skeleton\n" +
+				"'Need a hand?'");
             altEffect = ModPlayerFists.RegisterDashEffectID(DashEffects);
             skeleBroID = mod.ProjectileType<Projectiles.Skelebro>();
         }
