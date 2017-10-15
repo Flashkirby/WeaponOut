6ba66ac283f0214721790115b24310d928837772
Flash Kirby <flashkirby@gmail.com>
2017-10-15 01:33:27 +0100

    Add titanium fist

---
+++ Items/Weapons/Fists/FistsTitanium.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Graphics.Shaders;
+
+namespace WeaponOut.Items.Weapons.Fists
+{
+    [AutoloadEquip(EquipType.HandsOn, EquipType.HandsOff)]
+    public class FistsTitanium : ModItem
+    {
+        public override bool Autoload(ref string name) { return ModConf.enableFists; }
+        public static int altEffect = 0;
+        public static int shadowID = 0;
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Titanium Fist");
+            Tooltip.SetDefault(
+                "<right> to dash through enemies\n" +
+                "Dash grants 30% increased melee damage and knockback\n" +
+                "Combo grants shadow clones");
+            altEffect = ModPlayerFists.RegisterDashEffectID(DashEffects);
+            shadowID = mod.ProjectileType<Projectiles.SpiritShadow>();
+        }
+        public override void SetDefaults()
+        {
+            item.melee = true;
+            item.damage = 92; //205dps v 20def
+            item.useAnimation = 24; // 30%-50% reduction
+            item.knockBack = 7f;
+            item.tileBoost = 15; // Combo Power
+
+            item.value = Item.sellPrice(0, 1, 61, 0);
+            item.rare = 4;
+            item.shootSpeed = 10 + item.rare / 2;
+
+            item.UseSound = SoundID.Item19;
+            item.useStyle = ModPlayerFists.useStyle;
+            item.autoReuse = true;
+            item.noUseGraphic = true;
+            item.width = 20;
+            item.height = 20;
+        }
+        const int fistHitboxSize = 26;
+        const float fistDashSpeed = 8.5f;
+        const float fistDashThresh = 7f;
+        const float fistJumpVelo = 14.8f; // http://rextester.com/OIY60171
+        public bool AltStats(Player p) { return p.GetModPlayer<ModPlayerFists>().dashEffect == altEffect; }
+        const float altDashSpeed = 15f;
+        const float altDashThresh = 16f;
+        const float altJumpVelo = 16.85f;
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.TitaniumBar, 5);
+            recipe.AddTile(TileID.MythrilAnvil);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+        
+        /// <summary> The method called during a dash. Use for ongoing dust and gore effects. </summary>
+        public static void DashEffects(Player player, Item item)
+        {
+            if (player.dashDelay == 0) { }
+            for (int i = 0; i < 3; i++)
+            {
+                Dust d = Main.dust[Dust.NewDust(player.position, player.width, player.height, 111, -player.velocity.X, -player.velocity.Y, 0, default(Color), 0.8f)];
+                d.velocity *= 0.01f * Main.rand.Next(-20, 21);
+                d.shader = GameShaders.Armor.GetSecondaryShader(player.cShoe, player);
+            }
+        }
+
+        // Dash
+        public override void ModifyHitPvp(Player player, Player target, ref int damage, ref bool crit)
+        { float knockBack = 5f; ModifyHit(player, ref damage, ref knockBack, ref crit); }
+        public override void ModifyHitNPC(Player player, NPC target, ref int damage, ref float knockBack, ref bool crit)
+        { ModifyHit(player, ref damage, ref knockBack, ref crit); }
+        private void ModifyHit(Player player, ref int damage, ref float knockBack, ref bool crit)
+        {
+            if (AltStats(player))
+            {
+                damage = (int)(damage * 1.3);
+                knockBack *= 1.3f;
+            }
+        }
+        
+        // Combo
+        public override void HoldItem(Player player)
+        {
+            ModPlayerFists mpf = player.GetModPlayer<ModPlayerFists>();
+            if (mpf.IsComboActive)
+            {
+                bool noShadow = player.whoAmI == Main.myPlayer;
+                foreach (Projectile p in Main.projectile)
+                {
+                    if (p.active && p.owner == Main.myPlayer && p.type == shadowID)
+                    { noShadow = false; break; }
+                }
+                if (noShadow)
+                {
+                    Projectile.NewProjectile(player.Center, new Vector2(), shadowID, item.damage, item.knockBack, Main.myPlayer, 1f);
+                    Projectile.NewProjectile(player.Center, new Vector2(), shadowID, item.damage, item.knockBack, Main.myPlayer, -1f);
+                }
+            }
+        }
+
+        #region Hardmode Dash Base
+        public override bool CanUseItem(Player player)
+        {
+            if (player.altFunctionUse == 0)
+            {   // Short dash brings up to max default speed.
+                player.GetModPlayer<ModPlayerFists>().
+                SetDashOnMovement(fistDashSpeed, fistDashThresh, 0.992f, 0.96f, true, 0);
+            }
+            return true;
+        }
+        public override bool AltFunctionUse(Player player)
+        {
+            if (player.dashDelay == 0)
+            {
+                player.GetModPlayer<ModPlayerFists>().
+                    SetDash(altDashSpeed, altDashThresh, 0.992f, 0.96f, false, altEffect);
+                return true;
+            }
+            return false;
+        }
+        public override void UseItemHitbox(Player player, ref Rectangle hitbox, ref bool noHitbox)
+        {
+            if (!AltStats(player))
+            {
+                ModPlayerFists.UseItemHitbox(player, ref hitbox, fistHitboxSize, fistJumpVelo, 1f, 16f);
+            }
+            else
+            {
+                ModPlayerFists.UseItemHitbox(player, ref hitbox, fistHitboxSize, altJumpVelo, 1f, 16f);
+            }
+        }
+        #endregion
+
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
+        { ModPlayerFists.ModifyTooltips(tooltips, item); }
+    }
+}
---
+++ Projectiles/SpiritShadow.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using System;
+
+namespace WeaponOut.Projectiles
+{
+    public class SpiritShadow : ModProjectile
+    {
+        public override bool Autoload(ref string name) { return ModConf.enableFists; }
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Shadow Clone");
+        }
+        public override void SetDefaults()
+        {
+            projectile.width = Player.defaultWidth + 26 * 2;
+            projectile.height = Player.defaultHeight + 26 * 2;
+            projectile.alpha = 255;
+
+            projectile.penetrate = -1;
+            projectile.timeLeft = 60;
+
+            projectile.friendly = false;
+            projectile.melee = true;
+            projectile.tileCollide = false;
+            projectile.ignoreWater = true;
+
+            // So it doesn't conflict with fist NPC cooldown
+            projectile.usesLocalNPCImmunity = true;
+            projectile.localNPCHitCooldown = 20;
+        }
+
+        public override void AI()
+        {
+            projectile.friendly = false;
+            projectile.timeLeft++;
+            Player owner = Main.player[projectile.owner];
+            if (owner == null || !owner.active || owner.dead) { projectile.timeLeft = 0; return; }
+            if (owner.whoAmI == Main.myPlayer)
+            {
+                // Kill if no combo
+                if (!owner.GetModPlayer<ModPlayerFists>().IsComboActive)
+                {
+                    projectile.timeLeft = 0;
+                    if (Main.netMode != 0 && projectile.owner == owner.whoAmI)
+                    {
+                        projectile.netUpdate = true;
+                    }
+                }
+            }
+
+            projectile.velocity = owner.velocity;
+            projectile.Center = owner.Center + new Vector2(30 * projectile.ai[0], owner.gfxOffY) - projectile.velocity;
+
+            if (projectile.ai[0] == 1f)
+            {
+                owner.shadowDodgeCount++;
+                if (!owner.shadowDodge) owner.shadowDodgeCount++;// nullify effect of countdown
+                if (owner.shadowDodgeCount > 31) owner.shadowDodgeCount = 31;
+            }
+
+            projectile.friendly = owner.itemAnimation > 1 && owner.itemAnimation <= owner.itemAnimationMax - 6;
+        }
+    }
+}
