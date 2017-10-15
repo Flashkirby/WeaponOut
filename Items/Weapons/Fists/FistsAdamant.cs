65fe6b0bbc1639a53a2edd5d54192d2d2123c29d
Flash Kirby <flashkirby@gmail.com>
2017-10-15 01:32:52 +0100

    Add adamantite fist

---
+++ Items/Weapons/Fists/FistsAdamant.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Graphics.Shaders;
+
+namespace WeaponOut.Items.Weapons.Fists
+{
+    [AutoloadEquip(EquipType.HandsOn, EquipType.HandsOff)]
+    public class FistsAdamant : ModItem
+    {
+        public override bool Autoload(ref string name) { return ModConf.enableFists; }
+        public static int altEffect = 0;
+        public static int projID = 0;
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Adamantite Fist");
+            Tooltip.SetDefault(
+                "<right> to dash through enemies\n" +
+                "Dash grants 50% increased melee damage and knockback\n" +
+                "Combo grants destructive punches");
+            altEffect = ModPlayerFists.RegisterDashEffectID(DashEffects);
+            projID = mod.ProjectileType<Projectiles.SpiritExplosion>();
+        }
+        public override void SetDefaults()
+        {
+            item.melee = true;
+            item.damage = 107; //200dps v 20def
+            item.useAnimation = 29; // 30%-50% reduction
+            item.knockBack = 9f;
+            item.tileBoost = 9; // Combo Power
+
+            item.value = Item.sellPrice(0, 1, 38, 0);
+            item.rare = 4;
+            item.shootSpeed = 10 + item.rare / 2;
+
+            item.UseSound = SoundID.Item18;
+            item.useStyle = ModPlayerFists.useStyle;
+            item.autoReuse = true;
+            item.noUseGraphic = true;
+            item.width = 20;
+            item.height = 20;
+        }
+        const int fistHitboxSize = 28;
+        const float fistDashSpeed = 8f;
+        const float fistDashThresh = 7f;
+        const float fistJumpVelo = 14.4f; // http://rextester.com/OIY60171
+        public bool AltStats(Player p) { return p.GetModPlayer<ModPlayerFists>().dashEffect == altEffect; }
+        const float altDashSpeed = 16.5f;
+        const float altDashThresh = 16f;
+        const float altJumpVelo = 16.85f;
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.AdamantiteBar, 5);
+            recipe.AddTile(TileID.MythrilAnvil);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+        
+        /// <summary> The method called during a dash. Use for ongoing dust and gore effects. </summary>
+        public static void DashEffects(Player player, Item item)
+        {
+            if (player.dashDelay == 0) { }
+            for (int i = 0; i < 5; i++)
+            {
+                Dust d = Main.dust[Dust.NewDust(player.Center - new Vector2(8, 16), 8, 32, 
+                    31, -player.velocity.X, -player.velocity.Y, 0, new Color(1f, i * 0.2f, i * 0.2f), 0.8f)];
+                d.fadeIn = 1.1f + 0.1f * i;
+                d.noGravity = true;
+                d.velocity *= 0.2f;
+                d.shader = GameShaders.Armor.GetSecondaryShader(player.cShoe, player);
+            }
+        }
+
+        // Dash
+        public override void ModifyHitPvp(Player player, Player target, ref int damage, ref bool crit)
+        { float knockBack = 5f; ModifyHit(player, ref damage, ref knockBack, ref crit); }
+        public override void ModifyHitNPC(Player player, NPC target, ref int damage, ref float knockBack, ref bool crit)
+        { ModifyHit(player, ref damage, ref knockBack, ref crit); }
+        private void ModifyHit(Player player, ref int damage, ref float knockBack, ref bool crit)
+        {
+            if (AltStats(player))
+            {
+                damage = (int)(damage * 1.5);
+                knockBack *= 1.5f;
+            }
+        }
+
+        // Combo
+        public override void OnHitNPC(Player player, NPC target, int damage, float knockBack, bool crit)
+        {
+
+            ModPlayerFists mpf = player.GetModPlayer<ModPlayerFists>();
+            if (mpf.IsComboActiveItemOnHit)
+            {
+                if(!mpf.IsComboActive) Main.PlaySound(SoundID.Item21, player.Center);
+
+                Vector2 center = player.Center;
+                center += 1.5f * (target.Center - player.Center);
+                Projectile.NewProjectile(center, new Vector2(player.direction, player.gravDir), projID, 75, 11f, player.whoAmI);
+            }
+        }
+
+        // Idle Effect
+        private float soundDelay;
+        public override void HoldItem(Player player)
+        {
+            ModPlayerFists mpf = player.GetModPlayer<ModPlayerFists>();
+            if (mpf.IsComboActive)
+            {
+                soundDelay--;
+                if (soundDelay <= 0)
+                {
+                    Main.PlaySound(SoundID.Item22.WithVolume(0.3f), player.Center);
+                    soundDelay = 25;
+                }
+                Rectangle r = ModPlayerFists.GetPlayerOnHandRectangle(player, 4);
+
+                Dust d = Dust.NewDustDirect(r.TopLeft(), r.Width, r.Height, DustID.Smoke);
+
+                float dir = Main.time % 2 == 0 ? -1f : 1f;
+                d.position.X += dir * 4;
+                d.velocity = new Vector2(dir, -1f * player.gravDir);
+                d.shader = GameShaders.Armor.GetSecondaryShader(player.cHandOn, player);
+            }
+        }
+
+        #region Hardmode Dash Base
+        public override bool CanUseItem(Player player)
+        {
+            if (player.altFunctionUse == 0)
+            {   // Short dash brings up to max default speed.
+                player.GetModPlayer<ModPlayerFists>().
+                SetDashOnMovement(fistDashSpeed, fistDashThresh, 0.992f, 0.96f, true, 0);
+            }
+            return true;
+        }
+        public override bool AltFunctionUse(Player player)
+        {
+            if (player.dashDelay == 0)
+            {
+                player.GetModPlayer<ModPlayerFists>().
+                    SetDash(altDashSpeed, altDashThresh, 0.992f, 0.96f, false, altEffect);
+                return true;
+            }
+            return false;
+        }
+        public override void UseItemHitbox(Player player, ref Rectangle hitbox, ref bool noHitbox)
+        {
+            if (!AltStats(player))
+            {
+                ModPlayerFists.UseItemHitbox(player, ref hitbox, fistHitboxSize, fistJumpVelo, 1f, 16f);
+            }
+            else
+            {
+                ModPlayerFists.UseItemHitbox(player, ref hitbox, fistHitboxSize, altJumpVelo, 1f, 16f);
+            }
+        }
+        #endregion
+
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
+        { ModPlayerFists.ModifyTooltips(tooltips, item); }
+    }
+}
---
+++ Projectiles/SpiritExplosion.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using System;
+
+namespace WeaponOut.Projectiles
+{
+    public class SpiritExplosion : ModProjectile
+    {
+        public override bool Autoload(ref string name) { return true; }
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Adamantite Explosion");
+        }
+        public override void SetDefaults()
+        {
+            projectile.width = 80;
+            projectile.height = 80;
+            projectile.alpha = 255;
+
+            projectile.timeLeft = 3;
+            projectile.penetrate = -1;
+
+            projectile.friendly = true;
+            projectile.melee = true;
+            projectile.tileCollide = false;
+        }
+
+        public override void AI()
+        {
+            if (projectile.ai[0] == 0)
+            {
+                projectile.ai[0]++;
+                Main.PlaySound(SoundID.Item14.WithVolume(0.5f), projectile.position);
+
+                Dust d;
+                for (int i = 0; i < 8; i++)
+                {
+                    d = Dust.NewDustDirect(projectile.position, projectile.width, projectile.height, 31, 0f, 0f, 100, default(Color), 2f);
+                    d.velocity *= 3f;
+                    if (Main.rand.Next(2) == 0)
+                    {
+                        d.scale = 0.5f;
+                        d.fadeIn = 1f + (float)Main.rand.Next(10) * 0.1f;
+                    }
+                }
+                for (int i = 0; i < 14; i++)
+                {
+                    d = Dust.NewDustDirect(projectile.position, projectile.width, projectile.height, 31, 0f, 0f, 100, default(Color), 2f);
+                    d.velocity *= 5f;
+                    d = Dust.NewDustDirect(projectile.position, projectile.width, projectile.height, 6, 0f, 0f, 100, default(Color), 2f);
+                    d.velocity *= 2f;
+                }
+                Gore g;
+                for (int i = 0; i < 2; i++)
+                {
+                    for (int y = -1; y < 2; y += 2)
+                    {
+                        for (int x = -1; x < 2; x += 2)
+                        {
+                            g = Gore.NewGoreDirect(projectile.Center, default(Vector2), Main.rand.Next(61, 64), 1f);
+                            g.velocity *= 0.33f * i;
+                            g.velocity += new Vector2(x, y);
+                        }
+                    }
+                }
+            }
+        }
+
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            if (Main.player[projectile.owner].active && projectile.localAI[0] == 0)
+            {
+                Main.player[projectile.owner].GetModPlayer<ModPlayerFists>().ModifyComboCounter(1);
+                projectile.localAI[0]++;
+            }
+        }
+    }
+}
