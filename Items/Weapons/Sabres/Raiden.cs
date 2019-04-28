4bfd4fc8bf96b9e8d39c6b42dc3c969c50f2ac9b
Unknown <flashkirby@gmail.com>
2019-04-28 20:52:59 +0100

    Add Raiden check to unstick players for no-clipping targets

--- Items/Weapons/Sabres/Raiden.cs
+++ Items/Weapons/Sabres/Raiden.cs
@@ -19,11 +19,11 @@ namespace WeaponOut.Items.Weapons.Sabres
         private const float focusRadius = 320f;
         private const float focusRadiusReductionSpeedFactor = 15f;
 
-        public static int DustAmount(Player player) { return Main.clientPlayer == player ? 64 : 4; }
+        public static int DustAmount(Player player) { return player.whoAmI == Main.myPlayer ? 32 : 2; }
 
         public static float GetFocusRadius(Player player)
         {
-            float speedFactor = Math.Max(Math.Abs(player.velocity.X), Math.Abs(player.velocity.Y));
+            float speedFactor = Math.Max(Math.Abs(player.velocity.X), Math.Abs(player.velocity.Y / 2));
             return Math.Max(64, focusRadius - speedFactor * focusRadiusReductionSpeedFactor);
         }
 
@@ -90,7 +90,7 @@ namespace WeaponOut.Items.Weapons.Sabres
                     float distance = (center - npc.Center).Length();
                     if (distance <= radius)
                     {
-                        if (Collision.CanHitLine(center, 1, 1, npc.position, npc.width, npc.height))
+                        if (Collision.CanHit(center - new Vector2(12, 12), 24, 24, npc.position, npc.width, npc.height))
                         {
                             targets.Add(npc, distance);
                         }
@@ -254,258 +254,7 @@ namespace WeaponOut.Items.Weapons.Sabres
             d.position -= direction * 25f;
             d.velocity = direction * 5f;
         }
-        
-
-        //public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
-        //{
-        //    return false;
-        //}
-
-        //public override void HoldItem(Player player)
-        //{
-        //    int focus = getFocus(player);
-
-        //    // Reset style to swing when neutral
-        //    if (player.itemAnimation == 0)
-        //    {
-        //        item.useStyle = 1;
-
-        //        if ((Math.Abs(player.velocity.X) < 1.5f && player.velocity.Y == 0f
-        //        && player.grapCount == 0
-        //        && !player.pulley
-        //        && !(player.frozen || player.webbed || player.stoned || player.noItems)
-        //        )
-        //        || getFocus(player) >= focusTime)
-        //        {
-        //            if (getFocus(player) < focusTime + 1)
-        //            {
-        //                focus++;
-        //                updateFocus(player, focus);
-
-        //                Vector2 vector = player.Center;
-        //                vector.X += (float)Main.rand.Next(-2048, 2048) * 0.02f;
-        //                vector.Y += (float)Main.rand.Next(-2048, 2048) * 0.02f;
-
-
-        //                // Charging dust
-        //                Dust d = Main.dust[Dust.NewDust(
-        //                    vector, 1, 1,
-        //                    235, 0f, 0f, 0,
-        //                    Color.White, 1f)];
-
-        //                d.velocity *= 0f;
-        //                d.scale = Main.rand.Next(70, 85) * 0.01f;
-        //                // This dust uses fadeIn for homing into players
-        //                d.fadeIn = player.whoAmI + 1;
-        //            }
-        //        }
-        //        else
-        //        {
-        //            focus = 0;
-        //            updateFocus(player, focus);
-        //        }
-        //    }
-        //    else
-        //    {
-        //        if (player.itemAnimation == player.itemAnimationMax - 1)
-        //        {
-        //            int focusType = 0; // focus attack
-        //            if (!focusSlash)
-        //            {
-        //                if (slashFlip)
-        //                { focusType = -1; } //normal slash flipped
-        //                else
-        //                { focusType = 1; } //normal slash 
-        //            }
-        //            Projectile.NewProjectile(
-        //            player.position.X,
-        //            player.position.Y,
-        //            0, 0,
-        //            mod.ProjectileType<RaidenSlash>(), 
-        //            0, 0f,
-        //            player.whoAmI,
-        //            focusType);
-        //        }
-
-        //        item.useStyle = 0;
-        //        focus = 0;
-        //        updateFocus(player, focus);
-        //    }
-
-
-        //    if (focus >= focusTime)
-        //    {
-        //        if (focus == focusTime)
-        //        {
-        //            Main.PlaySound(2, player.position, 24);
-        //        }
-
-        //        int amount = 4;
-        //        int alpha = 200;
-        //        if (player.whoAmI == Main.myPlayer)
-        //        {
-        //            amount = 16;
-        //            alpha = 100;
-        //        }
-        //        // Range display dust;
-        //        for (int i = 0; i < amount; i++)
-        //        {
-        //            Vector2 offset = new Vector2();
-        //            double angle = Main.rand.NextDouble() * 2d * Math.PI;
-        //            offset.X += (float)(Math.Sin(angle) * focusRadius);
-        //            offset.Y += (float)(Math.Cos(angle) * focusRadius);
-        //            Dust dust = Main.dust[Dust.NewDust(
-        //                player.Center + offset - new Vector2(4, 4), 0, 0,
-        //                106, 0, 0, alpha, Color.White, 0.3f
-        //                )];
-        //            dust.velocity = player.velocity;
-        //            dust.fadeIn = 0.5f;
-        //            dust.noGravity = true;
-        //        }
-
-        //        // Display targets for client
-        //        if (player.whoAmI == Main.myPlayer)
-        //        {
-        //            List<NPC> targets = RaidenSlash.GetTargettableNPCs(player.Center, focusRadius);
-        //            Vector2 last = player.Center;
-        //            for (int i = 0; i < targets.Count; i++)
-        //            {
-        //                RaidenSlash.DrawDustToBetweenVectors(last, targets[i].Center, 106);
-        //                last = targets[i].Center;
-        //            }
-        //        }
-        //    }
-
-        //    if(player.itemAnimation == 1) // when almost done, swap around
-        //    { slashFlip = !slashFlip; }
-        //}
-
-        //public override bool HoldItemFrame(Player player) //called on player holding but not swinging
-        //{
-        //    if (getFocus(player) >= focusTime) //ready to slash
-        //    {
-        //        player.bodyFrame.Y = 4 * player.bodyFrame.Height;
-        //        return true;
-        //    }
-        //    return false;
-        //}
-
-        //public override void UseStyle(Player player)
-        //{
-        //    if(getFocus(player) >= focusTime)
-        //    {
-        //        focusSlash = true;
-        //        slashFlip = false;
-        //    }
-        //}
-
-        //public override bool UseItemFrame(Player player)
-        //{
-        //    //counts down from 1 to 0
-        //    float anim = player.itemAnimation / (float)(player.itemAnimationMax);
-        //    int frames = player.itemAnimationMax - player.itemAnimation;
-
-        //    // animation frames;
-        //    int start, swing, swing2, end;
-
-        //    if (slashFlip)
-        //    {
-        //        start = 4 * player.bodyFrame.Height;
-        //        swing = 3 * player.bodyFrame.Height;
-        //        swing2 = 2 * player.bodyFrame.Height;
-        //        end = 1 * player.bodyFrame.Height;
-        //    }
-        //    else
-        //    {
-        //        start = 1 * player.bodyFrame.Height;
-        //        swing = 2 * player.bodyFrame.Height;
-        //        swing2 = 3 * player.bodyFrame.Height;
-        //        end = 4 * player.bodyFrame.Height;
-        //    }
-
-        //    // Actual animation
-        //    if (anim > 0.9)
-        //    {
-        //        player.bodyFrame.Y = start;
-        //    }
-        //    else if (anim > 0.8f)
-        //    {
-        //        player.bodyFrame.Y = swing;
-        //    }
-        //    else if (anim > 0.7f)
-        //    {
-        //        player.bodyFrame.Y = swing2;
-        //    }
-        //    else
-        //    {
-        //        player.bodyFrame.Y = end;
-        //    }
-
-        //        return true;
-        //}
-
-        //private int minFrames = 6;
-        //public override void UseItemHitbox(Player player, ref Rectangle hitbox, ref bool noHitbox)
-        //{
-        //    // Set hitboxes accordingly
-        //    if (focusSlash)
-        //    {
-        //        noHitbox = !player.immuneNoBlink;
-        //        if (!noHitbox)
-        //        {
-        //            player.attackCD = 0;
-        //        }
-        //        hitbox = player.getRect();
-        //    }
-        //    else
-        //    {
-        //        if (player.itemAnimation == player.itemAnimationMax - 1) minFrames = 6;
-        //        NormalHitBox(player, ref hitbox, ref noHitbox, ref minFrames);
-        //    }
-        //}
-
-        //private static void NormalHitBox(Player player, ref Rectangle hitbox, ref bool noHitbox, ref int minFrames)
-        //{
-        //    float anim = player.itemAnimation / (float)player.itemAnimationMax;
-        //    // hit box only active during these frames
-        //    if ((anim <= 0.9f && anim > 0.7f) || minFrames >= 0)
-        //    {
-        //        int offsetX = 0;
-        //        if (anim > 0.8f) //first half of wing, covers behind
-        //        {
-        //            hitbox.Width = 64;
-        //            hitbox.Height = 92;
-        //            offsetX = 14;
-        //        }
-        //        else
-        //        {
-        //            hitbox.Width = 96;
-        //            hitbox.Height = 112;
-        //            offsetX = 40;
-        //        }
-        //        // Center hitbox and offset accordingly
-        //        hitbox.X = (int)player.Center.X - hitbox.Width / 2
-        //            + offsetX * player.direction;
-        //        hitbox.Y = (int)player.Center.Y - hitbox.Height / 2;
-
-        //        if (player.attackCD > 1) player.attackCD = 1;
-        //        minFrames--;
-        //    }
-        //    else
-        //    {
-        //        noHitbox = true;
-        //    }
-        //}
-
-        //public override void ModifyHitNPC(Player player, NPC target, ref int damage, ref float knockBack, ref bool crit)
-        //{
-        //    if(focusSlash)
-        //    {
-        //        damage *= 3;
-        //        crit = true;
-        //    }
-        //}
-    }
+            }
 
     public class RaidenSlash : ModProjectile
     {
@@ -567,10 +316,15 @@ namespace WeaponOut.Items.Weapons.Sabres
                     {
                         Main.PlaySound(SoundID.Item71, projectile.Center); sndOnce = false;
 
-                        //unstickCenter = player.Center;
+
+                        // Set up initial ending position as where we started
+                        if (player.whoAmI == Main.myPlayer)
+                        { endingPositionCenter = player.Center; }
+
+                        // Set ending slash direction
                         if (targets.Last().Center.X > player.Center.X)
                         { player.direction = 1; }
-                        else;
+                        else
                         { player.direction = -1; }
                     }
                 }
@@ -593,9 +347,11 @@ namespace WeaponOut.Items.Weapons.Sabres
         /// <summary> As there are 9 frames, gotta fit the number of targets within this window (roughly)</summary>
         public int totalTargetTime = 30;
         public List<NPC> targets = null;
-        //private Vector2? unstickCenter = null;
+        private Vector2? endingPositionCenter = null;
         public void ChargeSlashAI(Player player)
         {
+            if (targets.Count == 1) totalTargetTime = 15;
+
             setAnimationAndImmunities(player);
 
             float countf = targets.Count;
@@ -608,13 +364,9 @@ namespace WeaponOut.Items.Weapons.Sabres
             if (i >= framesPerTarget * countf)
             {
                 // End frame
+                player.Center = (Vector2)endingPositionCenter;
                 player.velocity = new Vector2(projectile.direction * -7.5f, player.gravDir * -2);
                 projectile.timeLeft = 0;
-
-                //if(unstickCenter != null && Main.clientPlayer == player)
-                //{
-                //    player.Center = (Vector2)unstickCenter;
-                //}
 
                 return;
             }
@@ -632,21 +384,6 @@ namespace WeaponOut.Items.Weapons.Sabres
                 { targetBottom = player.Bottom; target = null; }
                 else
                 { targetBottom = target.Bottom; }
-
-
-                //if ( Main.clientPlayer == player)
-                //{
-                //    // If can't reach this point, set an emergency "unstick" position
-                //    if (!Collision.CanHit(player.position, player.width, player.height, targetBottom, player.width, player.height))
-                //    { unstickCenter = player.Center; }
-                //    else
-                //    { unstickCenter = null; }
-
-                //    if(unstickCenter != null)
-                //    {
-                //        Main.NewText("can get stuck!");
-                //    }
-                //}
 
                 Vector2 oldBottom = new Vector2(player.Bottom.X, player.Bottom.Y);
                 Vector2 vecHeight = new Vector2(0, Player.defaultHeight / -2);
@@ -678,8 +415,41 @@ namespace WeaponOut.Items.Weapons.Sabres
                 ModSabres.RecentreSlash(projectile, player);
 
 
+                // Clientside unstick code, don't bother for others in MP
+                if (player.whoAmI == Main.myPlayer)
+                { UpdateValidEndingPosition(player); }
+                else { endingPositionCenter = player.Center; }
+
                 FrameCheck += oneFrame;
             }
+        }
+
+        private void UpdateValidEndingPosition(Player player)
+        {
+            // Check if the player is inside tiles
+            bool stuck = false;
+            try
+            {
+                Point targetTileBottom = player.Bottom.ToTileCoordinates();
+                for (int y = targetTileBottom.Y - 1; y >= targetTileBottom.Y - 3; y--)
+                {
+                    int x = targetTileBottom.X;
+
+                    //Main.NewText("check X " + x + " ? pX" + player.position.ToTileCoordinates().X);//DEBUG
+                    Dust.NewDustPerfect(new Vector2(x * 16 + 8, y * 16 + 8), 127);//DEBUG
+
+                    if (Main.tile[x, y] != null && Main.tile[x, y].active() && Main.tileSolid[Main.tile[x, y].type])
+                    {
+                        stuck = true; break;
+                    }
+                    if (stuck) break;
+                }
+            }
+            catch { stuck = true; }
+
+            // If we're fine, we can update the position
+            if (!stuck)
+            { endingPositionCenter = player.Center; }
         }
 
         private void setAnimationAndImmunities(Player player)
